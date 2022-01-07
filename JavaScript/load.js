91cc0a8dd0027a5dc9cec5b8b99203672424b967
pengxiao57 <s.pengxiao57@gmail.com>
2022-01-07 22:13:57 +0100

    [Accessibilité Jeux Vidéo] 1ère version du minisite

--- JavaScript/load.js
+++ JavaScript/load.js
@@ -1,3 +1,24 @@
-window.onload = function() {
-	
-}
+//window.onload = function() 
+$(document).ready(function ()
+{
+	$("header").load("https://pengxiao57.github.io/Accessibilite-des-Jeux-Video/Template/header.html");
+    $("nav").load("https://pengxiao57.github.io/Accessibilite-des-Jeux-Video/Template/nav.html");
+    $("footer").load("https://pengxiao57.github.io/Accessibilite-des-Jeux-Video/Template/footer.html");
+
+    $("#btn_stop").click(function()
+    {
+        var pause = $("#i_stop").attr("class").includes("pause");
+        console.log(pause)
+
+        if (pause)
+        {
+            $('#div_carousel').carousel("pause");
+            $("#i_stop").attr("class", "fa fa-play-circle fa-2x");
+        }
+        else
+        {
+            $('#div_carousel').carousel("cycle");
+            $("#i_stop").attr("class", "fa fa-pause-circle fa-2x");
+        }
+    });
+});
